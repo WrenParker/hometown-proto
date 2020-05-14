fb2f6f086c75d699538e5682d8ee2bf6931222d6
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-14 10:55:35 -0400

    implemented redux

--- hometown/frontend/src/components/App.js
+++ hometown/frontend/src/components/App.js
@@ -1,14 +1,16 @@
 import React, {Fragment} from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import store from '../store';
 import Nav from './views/Home/Home.js'
 
 function App() {
   return (
+    <Provider store={store}>
       <div id="app">
         <Nav />
       </div>
-    
+    </Provider>
   );
 }
 

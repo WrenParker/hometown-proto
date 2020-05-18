7fed988c23927c740a09dd74650682a86f3e5298
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-18 16:54:17 -0400

    added new urls for inventory

--- hometown/business/urls.py
+++ hometown/business/urls.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
-from .api import BusinessViewSet
+from .api import BusinessViewSet, KeywordViewSet, StoreFrontViewSet, InventoryViewSet
 
 router = routers.DefaultRouter()
-router.register('api/business', BusinessViewSet, 'business')
+router.register(r'api/business', BusinessViewSet, 'business')
+router.register(r'api/keywords', KeywordViewSet, 'keywords')
+router.register(r'api/storefront', StoreFrontViewSet, 'storefront')
+router.register(r'api/inventory', InventoryViewSet, 'inventory')
 
 urlpatterns = router.urls

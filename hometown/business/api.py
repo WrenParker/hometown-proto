36b670b619b19a5c390cee5c4e6482da2b59e875
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-18 16:19:48 -0400

    updated viewsets with new models

--- hometown/business/api.py
+++ hometown/business/api.py
@@ -1,6 +1,6 @@
-from business.models import Business
+from business.models import Business, Keyword, StoreFront, Item
 from rest_framework import viewsets, permissions
-from .serializers import BusinessSerializers
+from .serializers import BusinessSerializers, KeywordSerializer, StoreFrontSerializer, InventorySerializer
 
 class BusinessViewSet(viewsets.ModelViewSet):
   queryset = Business.objects.all()
@@ -8,3 +8,24 @@ class BusinessViewSet(viewsets.ModelViewSet):
     permissions.AllowAny
   ]
   serializer_class = BusinessSerializers
+
+class KeywordViewSet(viewsets.ModelViewSet):
+  queryset = Keyword.objects.all()
+  permission_classes = [
+    permissions.AllowAny
+  ]
+  serializer_class = KeywordSerializer
+
+class StoreFrontViewSet(viewsets.ModelViewSet):
+  queryset = StoreFront.objects.all()
+  permission_classes = [
+    permissions.AllowAny
+  ]
+  serializer_class = StoreFrontSerializer
+
+class InventoryViewSet(viewsets.ModelViewSet):
+  queryset = Item.objects.all()
+  permission_classes = [
+    permissions.AllowAny
+  ]
+  serializer_class = InventorySerializer

d171a882b46793119564d1bafc1b9e5819013cb2
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-18 16:54:07 -0400

    added serializers for inventory

--- hometown/business/serializers.py
+++ hometown/business/serializers.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
-from business.models import Business
+from business.models import Business, Keyword, StoreFront, Item
+
+
+
+class KeywordSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Keyword
+    fields = "__all__"
+
+class StoreFrontSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = StoreFront
+    fields = "__all__"
+
+class InventorySerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Item
+    fields = "__all__"
+
 
 class BusinessSerializers(serializers.ModelSerializer):
+  keywords = serializers.SlugRelatedField(
+    many=True,
+    read_only=False,
+    queryset=Keyword.objects.all(),
+    slug_field='keyword'
+  )
+
+  storefront = StoreFrontSerializer(many=True, read_only=True)
+  inventory = InventorySerializer(many=True, read_only=True)
+
   class Meta:
     model = Business
     fields = "__all__"

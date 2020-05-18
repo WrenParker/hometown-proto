196ed43340fcc982c3edf5459d199ac0af5a763f
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-18 16:54:57 -0400

    switched home app

--- hometown/frontend/src/components/App.js
+++ hometown/frontend/src/components/App.js
@@ -2,13 +2,13 @@ import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
-import Nav from './views/Home/Home.js'
+import Home from './views/Home/Home.js'
 
 function App() {
   return (
     <Provider store={store}>
       <div id="app">
-        <Nav />
+        <Home />
       </div>
     </Provider>
   );

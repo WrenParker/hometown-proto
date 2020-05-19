a6983f28a153ed0ab9a9db6023463a98ef3bab2e
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-19 11:09:40 -0400

    skeleton for storeview

---
+++ hometown/frontend/src/components/views/StoreView/components/StoreView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class StoreView extends Component {
+  render() {
+    return (
+      <Router>
+        
+      </Router>
+    )
+  }
+}
+
+export default StoreView

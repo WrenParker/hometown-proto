a29bd74726a1ae3b701672ba87cee98b0e2a1ce3
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-19 11:08:40 -0400

    implemented react router

--- hometown/frontend/src/components/App.js
+++ hometown/frontend/src/components/App.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
 import Home from './views/Home/Home.js'
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <Provider store={store}>
-      <div id="app">
-        <Home />
-      </div>
+      <Router>
+        <div id="app">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 }
+
 
 ReactDOM.render(<App />, document.getElementById('app'))

d18ba29a88c4b72d0c6d73154c0aa99ec6e5241e
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-19 09:21:27 -0400

    added nested relationships

--- hometown/business/models.py
+++ hometown/business/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 
 class Business(models.Model):
-  STORE_TYPES = (
-    ("RESTAURANT", "Restaurant"),
-    ("RETAIL", "Retail")
-  )
   
   name = models.CharField(max_length=100)
   description = models.CharField(max_length=100)
   owner = models.CharField(max_length=100)
   phone = models.CharField(max_length=10)
   email = models.EmailField(max_length=100)
-  storeType = models.CharField(max_length=100, choices=STORE_TYPES, null=True)
+  storeType = models.CharField(max_length=100, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
 
 class Keyword(models.Model):
@@ -20,13 +16,14 @@ class Keyword(models.Model):
 
 
 class StoreFront(models.Model):
-  business = models.ForeignKey(Business, on_delete=models.CASCADE)
+  business = models.ForeignKey(Business, related_name="storefront", on_delete=models.CASCADE)
   color = models.CharField(max_length=10)
+  location = models.CharField(max_length=20, null=True)
   img = models.ImageField(null=True)
 
 class Item(models.Model):
   business = models.ForeignKey(Business, related_name="inventory", on_delete=models.CASCADE)
   name = models.CharField(max_length = 25)
   description = models.CharField(max_length = 100)
-  available = models.IntegerField()
+  quantity = models.IntegerField(null=True)
   img = models.ImageField(null=True)

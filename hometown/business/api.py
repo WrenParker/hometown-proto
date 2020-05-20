db05ccc09ad30bab6535667b488d5ff49d280163
Wren Parker <wlp0003@mix.wvu.edu>
2020-05-20 13:48:39 -0400

    added permission class

--- hometown/business/api.py
+++ hometown/business/api.py
@@ -2,11 +2,24 @@ from business.models import Business, Keyword, StoreFront, Item
 from rest_framework import viewsets, permissions
 from .serializers import BusinessSerializers, KeywordSerializer, StoreFrontSerializer, InventorySerializer
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+          
+          return True
+
+        return obj.owner == request.user
+
 class BusinessViewSet(viewsets.ModelViewSet):
-  queryset = Business.objects.all()
+
   permission_classes = [
     permissions.AllowAny
   ]
+
+  queryset = Business.objects.all()
+
   serializer_class = BusinessSerializers
 
 class KeywordViewSet(viewsets.ModelViewSet):

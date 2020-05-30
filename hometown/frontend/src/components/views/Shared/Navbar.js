7314cd5f60c8345581a7f37834e775421e32efea
WrenParker <wrenparker@gmail.com>
2020-05-30 13:18:56 -0400

    added login check

--- hometown/frontend/src/components/views/Shared/Navbar.js
+++ hometown/frontend/src/components/views/Shared/Navbar.js
@@ -5,6 +5,36 @@ import './css/nav-style.css';
 
 
 export class Nav extends Component {
+  state={
+    auth: true,
+    accountName: "John"
+  }
+
+  componentDidMount() {
+    this.checkLogIn();
+  }
+
+  checkLogIn() {
+    let login = document.getElementById("account");
+    if(this.state.auth===true) {
+      let user = document.createElement("div");
+      user.innerHTML = `Hello, ${this.state.accountName}!`
+      let signOut = document.createElement("a");
+      signOut.addEventListener("click", this.changeRoute, false); //for later
+      signOut.innerHTML = `Sign out`;
+      login.appendChild(user);
+    }
+    else {
+      let user = document.createElement("a");
+      user.innerHTML = `Sign In / Register`
+      login.appendChild(user);
+
+    }
+  }
+
+  changeRoute() {
+    console.log("hi")
+  }
 
   render() {
     return (
@@ -12,14 +42,8 @@ export class Nav extends Component {
         <Navbar className="navbar" expand="lg">
           <div className="container">
             <Navbar.Brand href="#">Hometown</Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
-              <div className="link">
-                Sign In
-              </div>
-              {" / "}
-              <div className="link">
-                Register
-              </div>
+            <Navbar.Collapse id="account" className="justify-content-end">
+              
             </Navbar.Collapse>
           </div>
         </Navbar>
